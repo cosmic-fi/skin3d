8fb00588fa539697b719b95c1f5eea6cff573ee5
cosmic-fi <killrbid@gmail.com>
2025-06-08 05:33:51 +0300

    preview fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,28 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vite"
+import { fileURLToPath } from "url"
+import { dirname, resolve } from "path"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export default defineConfig({
+	root: resolve(__dirname, "demo"), // 👈 makes demo/ the working root
 	base: "./",
-	root: "demo",
+	publicDir: "public", // relative to new root (demo/public)
 	build: {
+		outDir: resolve(__dirname, "."), // 👈 output to project root
+		emptyOutDir: false, // don't delete vite.config.ts etc.
 		rollupOptions: {
-			input: {
-				main: "./demo/index.html"
-			},
-		},
+			output: {
+				entryFileNames: "assets/[name].js",
+				chunkFileNames: "assets/[name].js",
+				assetFileNames: "assets/[name].[ext]",
+			}
+		}
 	},
-});
+	resolve: {
+		alias: {
+			"@src": resolve(__dirname, "src")
+		}
+	}
+})
